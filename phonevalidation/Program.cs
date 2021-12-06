cddf1f6c708a0b8a2e830e9b08cc9a632bf046af
erfanrahmatzadeh <70009507+erfanrahmatzadeh@users.noreply.github.com>
2021-12-07 00:57:32 +0330

    Add project files.

---
+++ phonevalidation/Program.cs
@@ -0,0 +1,8 @@
+﻿var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
+var e164PhoneNumber = "+44 117 496 0123";
+var nationalPhoneNumber = "2024561111";
+var smsShortNumber = "83835";
+var phoneNumber = phoneNumberUtil.Parse(e164PhoneNumber, null);
+phoneNumber = phoneNumberUtil.Parse(nationalPhoneNumber, "US");
+phoneNumber = phoneNumberUtil.Parse(smsShortNumber, "US");
+Console.WriteLine(phoneNumber);
